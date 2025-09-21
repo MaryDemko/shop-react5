9d62ca2180e1f819785a74a87f4a7e9e011cf37e
MaryDemko <demkomary@mail.ru>
2025-09-21 23:08:05 +0400

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+const Header = () => {
+    return ( 
+    <header className="bg-white shadow-md">
+        <div className="container mx-auto flex juctify-between items-center p-4">
+            <Link 
+                to={'/'} 
+                className="text-2xl font-bold text-gray-600 hover: text-gray-800"
+            >
+                React Shop
+            </Link>
+            <nav>
+                <Link to={'/cart'} className="text-2xl font-bold text-gray-600 hover: text-gray-800">
+                    Корзина
+                </Link>
+            </nav>
+        </div>
+
+    </header>
+    )
+    
+}
+export default Header
