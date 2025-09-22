c4a44dc7e44ee0b6ee9b40071fa1d7ca0b362bf4
MaryDemko <demkomary@mail.ru>
2025-09-22 20:42:49 +0400

    add proect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
+import { Footer } from './components/Footer'
 
-function App() {
-  return <></>
-  
+import { Header } from './components/Header'
+
+import { Home } from './pages/Home'
+
+ export const App = () => {
+
+  return (
+
+    <Router>
+
+      <Header />
+
+      <main className='min-h-screen'>
+
+        <Routes>
+
+          <Route path='/' element={<Home />} />
+
+        </Routes>
+
+      </main>
+
+      <Footer />
+
+    </Router>
+
+  )
+
 }
-
-export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,20 @@
 import { Link } from "react-router-dom"
-const Header = () => {
-    return ( 
-    <header className="bg-white shadow-md">
-        <div className="container mx-auto flex juctify-between items-center p-4">
-            <Link 
-                to={'/'} 
-                className="text-2xl font-bold text-gray-600 hover: text-gray-800"
-            >
-                React Shop
-            </Link>
-            <nav>
-                <Link to={'/cart'} className="text-2xl font-bold text-gray-600 hover: text-gray-800">
-                    Корзина
-                </Link>
-            </nav>
-        </div>
+export const Header = () => { 
+    return ( <header className="bg-white shadow-md"> 
+    <div className="constainer mx-auto flex justify-between items-center p-4">
+        <Link to={"/"} className="text-2xl font-bold text-gray-600 hover:text-gray-800" >
+        React Shop
+        </Link>
+        <nav>
+            <Link to={"/cart"} className="text-gray-600 hover:text gray-800">
+             Корзина
+            
+        </Link>
+        </nav>
 
-    </header>
+    </div>
+
+        </header>
     )
-    
+
 }
-export default Header
