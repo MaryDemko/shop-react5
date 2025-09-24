54b421a04c79a4cf8c052b214a340b7dc1a01c7f
MaryDemko <demkomary@mail.ru>
2025-09-24 21:08:02 +0400

    add card, CartPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import { Footer } from './components/Footer'
-
 import { Header } from './components/Header'
-
 import { Home } from './pages/Home'
+import { CartPage } from './pages/CartPage'
+import { useState } from 'react'
 
  export const App = () => {
-
+  const [cartItems, setCartItems] = useState ([])
+  const onRemove = id => {
+    setCartItems(prev => prev.filter(item => item.id !==id))
+  }
   return (
-
     <Router>
-
       <Header />
-
       <main className='min-h-screen'>
-
         <Routes>
-
           <Route path='/' element={<Home />} />
-
+          <Route path='/cart' element={<CartPage cartItems={cartItems} onRemove={onRemove} />}
+          />
         </Routes>
-
       </main>
-
       <Footer />
-
     </Router>
-
   )
-
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
-
+import {ProductList} from '..//components/ProductList'
+import products from '../data/products'
 
 export const Home = () => {
 return (
-    <div>
+    <div className="container mx-auto p-4">
+        <ProductList products={products} />
         
     </div>
 )
